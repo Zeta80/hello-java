a7708d6a9e82e767c33a1e30c155226404a51c0d
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-07 14:32:29 +0100

    Add username input, with greetings

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,9 +1,17 @@
 package org.lessons.java;
-
+//importo scanner per leggere imput utente
+import java.util.Scanner;
 public class StampaSaluto {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
         System.out.println("This is my first Java repo");
+
+        //leggo imput utento e ricambio il saluto 🗿
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = input.nextLine();
+        System.out.println("Ciao, " + nome + "!");
     }
-}
+    }
+
+
